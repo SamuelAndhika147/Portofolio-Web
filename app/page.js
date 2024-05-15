83d45f27ad8eb99fa633335cda44c0b1fd95193b
SamuelAndhika147 <samuelandhikaprasetyo@smkwikrama.sch.id>
2024-05-15 13:41:43 +0700

    fix error 2

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-key */
 "use client";
 import { createContext, useState } from "react";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import ReactSwitch from "react-switch";
@@ -18,7 +18,6 @@ import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/swiper-bundle.css";
-import { metadata } from "./layout";
 
 export const ThemeContext = createContext(null);
 
